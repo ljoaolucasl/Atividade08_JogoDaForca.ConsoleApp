039296228bc401fd850737e3380aa02018c1a9f1
João <Joo@JOO>
2023-03-24 19:32:09 -0300

    Jogo funcionando com todas as funcionalidades pedidas
    Feito o refatoramento para melhor entendimento do código
    
    Feito validações para evitar erros
    
    Agora o jogo aprova letras sem acento para uma melhor dinâmica

--- Atividade08_JogoDaForcaConsoleApp/Program.cs
+++ Atividade08_JogoDaForcaConsoleApp/Program.cs
@@ -1,30 +1,84 @@
 ﻿
+using System.Globalization;
 using System.Linq;
+using System.Security.Authentication;
 
 namespace Atividade07.ConsoleApp
 {
     internal class Program
     {
+        static string palavraSecretaEscolhida;
+
         static void Main(string[] args)
         {
-            string[] tracosForca;
             char[] letrasPalavraSecreta;
-            string[] palavrasSecretas = { "ABACATE", "ABACAXI", "ACEROLA" };
-            string palavraSecretaEscolhida;
-            int posicaoLetraAcertada;
-            int contadorErro = 0;
 
-            palavraSecretaEscolhida = palavrasSecretas[0];
+            int contadorErro;
 
-            tracosForca = new string[palavraSecretaEscolhida.Length];
+            bool continuaJogo, acerto, jogarNovamente;
 
-            for (int i = 0; i < tracosForca.Length; i++)
+            char[] historicoLetrasEscolhidas = new char[6];
+
+            do
             {
-                tracosForca[i] = "_";
-            }
+                continuaJogo = true;
+                contadorErro = 0;
+                Array.Clear(historicoLetrasEscolhidas, 0, 6);
 
-            letrasPalavraSecreta = palavraSecretaEscolhida.ToCharArray();
+                GerarBemVindoDescricao();
 
+                palavraSecretaEscolhida = GerarPalavraSecretaAleatoria();
+
+                GerarForca();
+
+                letrasPalavraSecreta = GerarTracosPalavraSecreta();
+
+                while (continuaJogo)
+                {
+                    char letraEscolhida = CapturarLetraEscolhida();
+
+                    acerto = AnalisarLetraAcertoOuErro(ref letrasPalavraSecreta, letraEscolhida);
+
+                    CasoLetraAcertada(letrasPalavraSecreta, acerto, ref continuaJogo);
+
+                    MostradorDeErros(contadorErro, acerto, historicoLetrasEscolhidas, letraEscolhida);
+
+                    CasoLetraErrada(ref contadorErro, acerto, ref continuaJogo);
+                }
+
+                jogarNovamente = JogarNovamente();
+
+            } while (jogarNovamente);
+        }
+
+        private static void GerarBemVindoDescricao()
+        {
+            Console.Clear();
+            Console.WriteLine(" ╔══════════════════════════════════════╗");
+            Console.WriteLine(" ║  * Bem-Vindo(a) ao Jogo Da Forca! *  ║");
+            Console.WriteLine(" ╠══════════════════════════════════════╣");
+            Console.WriteLine(" ║          * Nome de Frutas *          ║");
+            Console.WriteLine(" ╚══════════════════════════════════════╝");
+        }
+
+        private static string GerarPalavraSecretaAleatoria()
+        {
+            Console.WriteLine();
+
+            int numeroAleatorio;
+            string[] palavrasSecretas = { "ABACATE", "ABACAXI", "ACEROLA", "AÇAÍ", "ARAÇA", "BACABA", "BACURI", "BANANA", "CAJÁ",
+                "CAJÚ", "CARAMBOLA", "CUPUAÇU", "GRAVIOLA", "GOIABA", "JABUTICABA", "JENIPAPO", "MAÇÃ", "MANGABA", "MANGA",
+                "MARACUJÁ", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI", "TANGERINA", "UMBU", "UVA", "UVAIA" };
+
+            Random GerarNumeroAleatorio = new();
+
+            numeroAleatorio = GerarNumeroAleatorio.Next(0, 29);
+
+            return palavrasSecretas[numeroAleatorio];
+        }
+
+        private static void GerarForca()
+        {
             Console.WriteLine("  ╔╦═════════════════");
             Console.WriteLine("  ╠╝                │");
             Console.WriteLine("  ║                 │");
@@ -39,39 +93,145 @@ namespace Atividade07.ConsoleApp
             Console.WriteLine("  ║");
             Console.WriteLine("  ║");
             Console.WriteLine("  ║");
-            Console.Write("  ╨");
-            Console.WriteLine(string.Join(" ", tracosForca));
+            Console.Write("  ╨ ");
+        }
 
-            while (true)
+        private static char[] GerarTracosPalavraSecreta()
+        {
+            char[] letrasPalavraSecreta = new char[palavraSecretaEscolhida.Length];
+            for (int i = 0; i < letrasPalavraSecreta.Length; i++)
             {
-                Console.SetCursorPosition(0, 15);
+                letrasPalavraSecreta[i] = '_';
+            }
+
+            Console.WriteLine(string.Join(" ", letrasPalavraSecreta));
+            return letrasPalavraSecreta;
+        }
+
+        private static char CapturarLetraEscolhida()
+        {
+            bool verificaLetras;
+            bool verificaChar;
+            bool verificaNumero;
+            char letra;
+            do
+            {
+                Console.SetCursorPosition(0, 21);
                 Console.Write("\nEscolha uma letra: ");
-                Console.SetCursorPosition(19, 16);
-                
-                string letraEscolhida = Console.ReadLine();
+                Console.SetCursorPosition(19, 22);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(19, 22);
 
-                posicaoLetraAcertada = Array.FindIndex(letrasPalavraSecreta, letra => letra == Convert.ToChar(letraEscolhida));
+                string entrada = Console.ReadLine().ToUpper();
 
-                if (posicaoLetraAcertada != -1)
+                verificaChar = char.TryParse(entrada, out letra);
+
+                verificaNumero = int.TryParse(entrada, out int numero);
+
+                verificaLetras = entrada.All(char.IsLetter);
+
+            } while (!verificaChar || verificaNumero || !verificaLetras);
+
+            return letra;
+        }
+
+        private static bool AnalisarLetraAcertoOuErro(ref char[] letrasPalavraSecreta, char letraEscolhida)
+        {
+            bool acerto = false;
+            for (int i = 0; i < palavraSecretaEscolhida.Length; i++)
+            {
+                if (String.Compare(Convert.ToString(palavraSecretaEscolhida[i]), Convert.ToString(letraEscolhida),
+                    CultureInfo.CurrentCulture, CompareOptions.IgnoreNonSpace) == 0)
                 {
-                    tracosForca[posicaoLetraAcertada] = letraEscolhida;
-                    Console.SetCursorPosition(3, 14);
-                    Console.WriteLine(string.Join(" ", tracosForca));
-                }
-                else
-                {
-                    contadorErro++;
-
-                    switch (contadorErro)
-                    {
-                        case 1: Console.SetCursorPosition(0, 3); Console.WriteLine("  ║                 O"); break;
-                        case 2: Console.SetCursorPosition(0, 4); Console.WriteLine("  ║                 x"); break;
-                        case 3: Console.SetCursorPosition(0, 4); Console.WriteLine("  ║                ╱x╲"); break;
-                        case 4: Console.SetCursorPosition(0, 5); Console.WriteLine("  ║                ╱ "); break;
-                        case 5: Console.SetCursorPosition(0, 5); Console.WriteLine("  ║                ╱ ╲"); break;
-                    }
+                    letrasPalavraSecreta[i] = palavraSecretaEscolhida[i];
+                    acerto = true;
                 }
             }
+
+            return acerto;
+        }
+
+        private static void CasoLetraAcertada(char[] letrasPalavraSecreta, bool acerto, ref bool continuaJogo)
+        {
+            if (acerto)
+            {
+                Console.SetCursorPosition(4, 20);
+                Console.WriteLine(string.Join(" ", letrasPalavraSecreta));
+
+                string validarPalavra = new(letrasPalavraSecreta);
+
+                if (validarPalavra == palavraSecretaEscolhida)
+                {
+                    Console.SetCursorPosition(0, 23);
+                    MudarCorTexto(ConsoleColor.Green, "Parabéns! Você acertou!\n");
+                    continuaJogo = false;
+                    acerto = false;
+                }
+            }
+        }
+
+        private static void MostradorDeErros(int contadorErro, bool acerto, char[] historicoLetrasEscolhidas, char letraEscolhida)
+        {
+            if (!acerto)
+            {
+                historicoLetrasEscolhidas[contadorErro] = letraEscolhida;
+                Console.SetCursorPosition(25, 6);
+                Console.WriteLine(string.Join(" ", historicoLetrasEscolhidas));
+                Console.SetCursorPosition(25, 8);
+                Console.WriteLine($"5/{contadorErro + 1}");
+            }
+        }
+
+        private static void CasoLetraErrada(ref int contadorErro, bool acerto, ref bool continuaJogo)
+        {
+            if (!acerto)
+            {
+                contadorErro++;
+
+                switch (contadorErro)
+                {
+                    case 1: Console.SetCursorPosition(0, 9); Console.WriteLine("  ║                 O"); break;
+                    case 2: Console.SetCursorPosition(0, 10); Console.WriteLine("  ║                 x"); break;
+                    case 3: Console.SetCursorPosition(0, 10); Console.WriteLine("  ║                 x\\"); break;
+                    case 4: Console.SetCursorPosition(0, 10); Console.WriteLine("  ║                /x\\"); break;
+                    case 5:
+                        Console.SetCursorPosition(0, 11); Console.WriteLine("  ║                / \\");
+                        continuaJogo = false;
+                        Console.SetCursorPosition(0, 23);
+                        MudarCorTexto(ConsoleColor.Red, "Você perdeu, a palavra secreta era ");
+                        Console.WriteLine($"\"{palavraSecretaEscolhida}\"");
+                        break;
+                }
+            }
+        }
+
+        private static bool JogarNovamente()
+        {
+            string resposta;
+            bool jogarNovamente;
+
+            Console.WriteLine("Quer Jogar Novamente? (S/N)");
+
+            do
+            {
+                Console.SetCursorPosition(0, 25);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, 25);
+                resposta = Console.ReadLine().ToUpper();
+
+            } while (resposta != "S" && resposta != "N");
+
+            if (resposta == "S") { jogarNovamente = true; }
+            else { jogarNovamente = false; Console.WriteLine("\nFinalizando Aplicação . . ."); }
+
+            return jogarNovamente;
+        }
+
+        private static void MudarCorTexto(ConsoleColor cor, string mensagem)
+        {
+            Console.ForegroundColor = cor;
+            Console.Write(mensagem);
+            Console.ResetColor();
         }
     }
 }
